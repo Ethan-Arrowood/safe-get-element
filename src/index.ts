7e751262ca75dbc2a82c3a9181b15d1e20af02e0
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-09-30 16:52:28 -0400

    use existing map and work on testing

--- src/index.ts
+++ src/index.ts
@@ -1,163 +1,5 @@
-type TagNames = 'button' | 'input' | 'select' | 'textarea' | 'tbody';
-
-type TagNameMap<T extends TagNames> =
-  // Main root
-  T extends 'html' ? HTMLHtmlElement
-    // Document metadata
-    : T extends 'base' ? unknown
-    : T extends 'head' ? unknown
-    : T extends 'link' ? unknown
-    : T extends 'meta' ? unknown
-    : T extends 'style' ? unknown
-    : T extends 'title' ? unknown
-    // Sectioning root
-    : T extends 'body' ? unknown
-    // Content sectioning
-    : T extends 'address' ? unknown
-    : T extends 'article' ? unknown
-    : T extends 'aside' ? unknown
-    : T extends 'footer' ? unknown
-    : T extends 'header' ? unknown
-    : T extends 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' ? unknown
-    : T extends 'main' ? unknown
-    : T extends 'nav' ? unknown
-    : T extends 'section' ? unknown
-    // Text content
-    : T extends 'blockquote' ? unknown
-    : T extends 'dd' ? unknown
-    : T extends 'div' ? unknown
-    : T extends 'dl' ? unknown
-    : T extends 'dt' ? unknown
-    : T extends 'figcaption' ? unknown
-    : T extends 'figure' ? unknown
-    : T extends 'hr' ? unknown
-    : T extends 'li' ? unknown
-    : T extends 'ol' ? unknown
-    : T extends 'p' ? unknown
-    : T extends 'pre' ? unknown
-    : T extends 'ul' ? unknown
-    // Inline text semantics
-    : T extends 'a' ? unknown
-    : T extends 'abbr' ? unknown
-    : T extends 'b' ? unknown
-    : T extends 'bdi' ? unknown
-    : T extends 'bdo' ? unknown
-    : T extends 'br' ? unknown
-    : T extends 'cite' ? unknown
-    : T extends 'code' ? unknown
-    : T extends 'data' ? unknown
-    : T extends 'dfn' ? unknown
-    : T extends 'em' ? unknown
-    : T extends 'i' ? unknown
-    : T extends 'kbd' ? unknown
-    : T extends 'mark' ? unknown
-    : T extends 'q' ? unknown
-    : T extends 'rp' ? unknown
-    : T extends 'rt' ? unknown
-    : T extends 'ruby' ? unknown
-    : T extends 's' ? unknown
-    : T extends 'samp' ? unknown
-    : T extends 'small' ? unknown
-    : T extends 'span' ? unknown
-    : T extends 'strong' ? unknown
-    : T extends 'sub' ? unknown
-    : T extends 'sup' ? unknown
-    : T extends 'time' ? unknown
-    : T extends 'u' ? unknown
-    : T extends 'var' ? unknown
-    : T extends 'wbr' ? unknown
-    // Image and multimedia
-    : T extends 'area' ? unknown
-    : T extends 'audio' ? unknown
-    : T extends 'img' ? unknown
-    : T extends 'map' ? unknown
-    : T extends 'track' ? unknown
-    : T extends 'video' ? unknown
-    // Embedded content
-    : T extends 'embed' ? unknown
-    : T extends 'iframe' ? unknown
-    : T extends 'object' ? unknown
-    : T extends 'param' ? unknown
-    : T extends 'picture' ? unknown
-    : T extends 'portal' ? unknown
-    : T extends 'source' ? unknown
-    // SVG and MathML
-    : T extends 'svg' ? unknown
-    : T extends 'math' ? unknown
-    // Scripting
-    : T extends 'canvas' ? unknown
-    : T extends 'noscript' ? unknown
-    : T extends 'script' ? unknown
-    // Demarcating edits
-    : T extends 'del' ? unknown
-    : T extends 'ins' ? unknown
-    // Table content
-    : T extends 'caption' ? unknown
-    : T extends 'col' ? unknown
-    : T extends 'colgroup' ? unknown
-    : T extends 'table' ? unknown
-    : T extends 'tbody' ? unknown
-    : T extends 'math' ? unknown
-    : T extends 'td' ? unknown
-    : T extends 'tfoot' ? unknown
-    : T extends 'th' ? unknown
-    : T extends 'thead' ? unknown
-    : T extends 'tr' ? unknown
-    // Forms
-    : T extends 'button' ? unknown
-    : T extends 'datalist' ? unknown
-    : T extends 'fieldset' ? unknown
-    : T extends 'form' ? unknown
-    : T extends 'input' ? unknown
-    : T extends 'label' ? unknown
-    : T extends 'legend' ? unknown
-    : T extends 'meter' ? unknown
-    : T extends 'optgroup' ? unknown
-    : T extends 'option' ? unknown
-    : T extends 'output' ? unknown
-    : T extends 'progress' ? unknown
-    : T extends 'select' ? unknown
-    : T extends 'textarea' ? unknown
-    // Interactive elements
-    : T extends 'details' ? unknown
-    : T extends 'dialog' ? unknown
-    : T extends 'menu' ? unknown
-    : T extends 'summary' ? unknown
-    // Web Components
-    : T extends 'slot' ? unknown
-    : T extends 'template' ? unknown
-    // Obsolete and deprecated elements
-    : T extends 'acronym' ? unknown
-    : T extends 'applet' ? unknown
-    : T extends 'basefont' ? unknown
-    : T extends 'bgsound' ? unknown
-    : T extends 'big' ? unknown
-    : T extends 'blink' ? unknown
-    : T extends 'center' ? unknown
-    : T extends 'content' ? unknown
-    : T extends 'dir' ? unknown
-    : T extends 'font' ? unknown
-    : T extends 'frame' ? unknown
-    : T extends 'frameset' ? unknown
-    : T extends 'hgroup' ? unknown
-    : T extends 'image' ? unknown
-    : T extends 'keygen' ? unknown
-    : T extends 'marquee' ? unknown
-    : T extends 'menuitem' ? unknown
-    : T extends 'nobr' ? unknown
-    : T extends 'noembed' ? unknown
-    : T extends 'noframes' ? unknown
-    : T extends 'plaintext' ? unknown
-    : T extends 'rb' ? unknown
-    : T extends 'rtc' ? unknown
-    : T extends 'shadow' ? unknown
-    : T extends 'spacer' ? unknown
-    : T extends 'strike' ? unknown
-    : T extends 'tt' ? unknown
-    : T extends 'xmp' ? unknown
-    : never
-
-function assertElement <TagName extends TagNames> (element: HTMLElement, tagName: TagName): element is TagNameMap<TagName> {
+type TagNames = keyof HTMLElementTagNameMap;
+function assertElement <TagName extends TagNames> (element: HTMLElement, tagName: TagName): element is HTMLElementTagNameMap[TagName] {
   if (element.tagName === tagName.toUpperCase()) {
     return true
   } else {
@@ -166,7 +8,7 @@ function assertElement <TagName extends TagNames> (element: HTMLElement, tagName
 }
 
 export function safeGetElement (id: string): HTMLElement
-export function safeGetElement <TagName extends TagNames>(id: string, assertType: TagName): TagNameMap<TagName>
+export function safeGetElement <TagName extends TagNames>(id: string, assertType: TagName): HTMLElementTagNameMap[TagName]
 export function safeGetElement <TagName extends TagNames>(id: string, assertType?: TagName) {
   const element = document.getElementById(id)
 
@@ -175,6 +17,6 @@ export function safeGetElement <TagName extends TagNames>(id: string, assertType
   }
 
   return assertType !== undefined && assertElement(element, assertType)
-    ? element as TagNameMap<TagName>
+    ? element as HTMLElementTagNameMap[TagName]
     : element as HTMLElement
 }
--- test/index.ts
+++ test/index.ts
@@ -1,15 +1,21 @@
 import tap from 'tap'
 import { JSDOM } from 'jsdom'
+import { tagNames } from './tagNames'
 import { safeGetElement } from '../src/index'
 
 tap.before(() => {
-  const dom = new JSDOM(`<!DOCTYPE html><button id='button'></button>`)
+  const html = tagNames.reduce((acc, tag) => {
+    return acc += `\n<${tag} id='${tag}-id'></${tag}>`
+  }, '<!DOCTYPE html>')
+  const dom = new JSDOM(html)
   global.document = dom.window.document
 })
 
 tap.test('safeGetElement', t => {
   t.throws(() => safeGetElement('does-not-exist'))
-  t.throws(() => safeGetElement('button', 'input'))
-  t.doesNotThrow(() => safeGetElement('button', 'button'))
+  // t.throws(() => safeGetElement('button', 'input'))
+  tagNames.forEach(tag => {
+    t.doesNotThrow(() => safeGetElement(`${tag}-id`, tag))
+  })
   t.end()
 })
